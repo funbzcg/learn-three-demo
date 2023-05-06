3f3e215d105602fe31d9ee5f219c609752da613f
funbzcg <5441714+funbzcg@user.noreply.gitee.com>
2023-05-06 20:30:01 +0800

    建立三维坐标系

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# learn-three-demo
-three.js learnning
+# three.js 三要素
+
+## 三维场景 scene
+
+### 集合体 Geometry
+
+### 材质 Material
+
+    构造器传入一个 option
+
+### 网格模型 Mesh
+
+    构造器传入两个参数（几何体实例，材质实例）
+
+### 坐标系 AxesHelper
+
+坐标轴颜色红 R、绿 G、蓝 B 分别对应坐标系的 x、y、z 轴，对于 three.js 的 3D 坐标系默认 y 轴朝上。
+
+## 照相机 camera
+
+    传入四个参数（视锥体上下角度，宽高比，近景切面，远景切面）
+
+## 渲染器 Renderer
+
+    构造器传入两个参数（网格模型，照相机）
--- index.js
+++ index.js
@@ -1,28 +1,37 @@
-import * as T3 from "three"
-import { OrbitControls } from "three/addons/controls/OrbitControls.js"
+/*
+ * @Date: 2023-05-06 18:37:29
+ * @LastEditTime: 2023-05-06 20:29:28
+ * @Description:
+ */
+import * as T3 from "three";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 
 const width = 800;
 const height = 500;
 /**  创建一个三维场景 */
 const scene = new T3.Scene();
 window.scene = scene;
-
+/** 创建三维坐标系 */
+const axesHelper = new T3.AxesHelper(150);
+/**添加到三维场景 */
+scene.add(axesHelper);
 /** 创建 */
 const box = new T3.BoxGeometry(100, 100, 100);
 /** 创建材质 */
 const material = new T3.MeshBasicMaterial({
-  color: "#00ff00"
-})
+  color: "#00ff00", //设置材质颜色
+  transparent: true, //开启透明
+  opacity: 0.5, //设置透明度
+});
 /** 创建一个网格模型 */
-const mesh = new T3.Mesh(box, material)
+const mesh = new T3.Mesh(box, material);
 // 集合体位置设置
-mesh.position.set(1, 1, 0)
+mesh.position.set(1, 1, 0);
 // 将模型添加到场景中
 scene.add(mesh);
 
-
 /** 创建透视相机 */
-const camera = new T3.PerspectiveCamera(30, width / height, 0.1, 3000);
+const camera = new T3.PerspectiveCamera(30, width / height, 1, 3000);
 // 相机位置
 camera.position.set(600, 600, 600);
 
@@ -33,6 +42,5 @@ camera.lookAt(mesh.position);
 const render = new T3.WebGLRenderer();
 render.setSize(width, height);
 render.render(scene, camera);
-document.body.appendChild(render.domElement)
-
-
+const webgl = document.getElementById("webgl");
+webgl.appendChild(render.domElement);
